c33ccbfdf21d8ea2150c5388bbea74670f1b913e
Meyer <viniciusmeyerlaroza@gmail.com.br>
2022-10-19 20:29:31 -0300

    Forget

---
+++ app/src/main/java/ads/pdm/mycontacts/Model/Contact.kt
@@ -0,0 +1,14 @@
+package ads.pdm.mycontacts.Model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Contact(
+    val id: Int,
+    var name: String,
+    var adress: String,
+    var phone: String,
+    var email: String,
+):Parcelable
+
